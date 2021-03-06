293832192f10dee39865e2ae43e0e7e41a80b362
Samuel Nwankwo <nwankwosa@gmail.com>
2021-03-06 16:39:11 -0500

    Changed appbar title

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
 
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Edit Profile'),
     );
   }
 }
